299e3158ec51771bfacb91cb82c02758e75ed03c
esk <santhu.esk@gmail.com>
2018-08-12 17:13:28 +0100

    Substraction

--- SayHello.sh
+++ SayHello.sh
@@ -4,5 +4,8 @@ echo "Hello!!!";
 STR="Hello World!";
 echo $STR;
 
-#Add two numbers
-echo $((1+2));
+# Add two numbers
+echo "Sum of 1 and 2 is $((1+2))";
+
+# Subtract two numbers
+echo "Difference of 100 and 90 is $((100-90))";
